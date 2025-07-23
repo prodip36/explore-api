40dbafad8014104372671e6517ea7f98b02175d0
nothing36 <prodipsarker953@gmail.com>
2025-07-23 15:09:51 +0600

    post section added

---
+++ js/post.js
@@ -0,0 +1,20 @@
+function loadPost(){
+    const url='https://jsonplaceholder.typicode.com/posts';
+    fetch(url)
+    .then(res => res.json())
+    .then(post => showPost(post));
+}
+function showPost(post)
+{
+    const postContainer= document.getElementById('post-container');
+    for(const data of post){
+        const postDiv= document.createElement('div');
+        postDiv.innerHTML= `
+        <h4>User- ${data.id}</h4>
+        <h5>Post: ${data.title}<h5>
+        <p>Post Description: ${data.body}</p>
+        <br> <br>
+        `
+        postContainer.appendChild(postDiv);
+    }
+}
